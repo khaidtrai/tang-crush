7270672ddb84d137d8a9035bb8cef10b326d109b
khaidtrai <88873109+khaidtrai@users.noreply.github.com>
2021-08-13 12:30:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    introTitle: 'Cục cưng của anh!',
+    introDesc: `Tỉ sửu dần mẹo thình tị ngọ mùi hương của em làm anh chấn thương vì vấn vương`,
+    btnIntro: 'hehe',
+    title: 'Love u pặc pặc 😙',
+    desc: 'Pikapu Mai Hương xỉnh xẻo của anh ',
+    btnYes: 'Vẫn cứ là yêu anh nhỉ <3',
+    btnNo: 'Bùn qué bùn qué',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Mai gặp được nhau tặng cho ly trà bử thơm ngon nhé .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
